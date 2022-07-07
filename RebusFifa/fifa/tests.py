aaa5ea5f2ac612576f9fefa21ca5511b8de6e70a
rogergogo <roger.gomez@elenas.la>
2022-07-07 08:27:52 -0500

    Clean

--- RebusFifa/fifa/tests.py
+++ RebusFifa/fifa/tests.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from faker import Faker, providers
+from faker import Faker
 from .models import Team, Player, Staff, Person
 
 
